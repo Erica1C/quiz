b67c1d3b8c884e95e5c2d76f5daebee5f23c8a3f
Erica2D <147394713+Erica1C@users.noreply.github.com>
2024-10-31 12:03:13 -0300

    bn

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 function criarCartao(pergunta,resposta){
     let conteudo = document.getElementById("conteudo")
     let cartao = document.createElement()
+    cartao.classList = 'cartao'
+
+    cartao.innerHTML = 'TESTANDO O INNER'
+
+    conteudo.appendChild(cartao)
 }
 criarCartao(1,2)
