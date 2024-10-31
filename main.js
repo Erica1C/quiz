66caa924b37d7a06d4b6f446f7f34b4f2a1a0519
Erica2D <147394713+Erica1C@users.noreply.github.com>
2024-10-31 12:10:35 -0300

    xbn

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ function criarCartao(pergunta,resposta){
     let cartao = document.createElement('aeticle')
     cartao.classList = 'cartao'
 
-    cartao.innerHTML = `<div class="cartao--conteudo">
+    cartao.innerHTML = `
+    <div class="cartao--conteudo">
                 <div class="cartao--pergunta"><p>Qual o maior animal aquático do mundo?</p></div>
                 <div class="cartao--resposta"><p>Baleia-azul.</p></div>
             </div>
