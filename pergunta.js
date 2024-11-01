cc0440d120bfcce98b01dce79d9f30a1c2ffd682
Erica2D <147394713+Erica1C@users.noreply.github.com>
2024-11-01 08:41:15 -0300

    .

--- pergunta.js
+++ pergunta.js
@@ -3,22 +3,22 @@ criarCartao(
     "Baleia-azul."
 )
 criarCartao(
-    "O que acontece se a Estrela-do-mar perder um de seus braços?"
+    "O que acontece se a Estrela-do-mar perder um de seus braços?",
     "Ele se regenera."
 )
 criarCartao(
-    "Por que os Polvos soltam uma espécie de tinta?"
+    "Por que os Polvos soltam uma espécie de tinta?",
     "Técnica utilizada para fugir ou confundir predadores."
 )
 criarCartao(
-    "A Orca não é uma baleia. Qual é sua espécie?"
+    "A Orca não é uma baleia. Qual é sua espécie?",
     "As orcas são golfinhos."
 )
 criarCartao(
-    "Qual é o tipo de alimentação feita pelo Tubarão-Baleia?"
+    "Qual é o tipo de alimentação feita pelo Tubarão-Baleia?",
     "Pequenos crustáceos, peixes que nadam em cardume e, ocasionalmente, atum e lulas."
 )
 criarCartao(
-    "Quais são as baleias mais dóceis do oceano?"
+    "Quais são as baleias mais dóceis do oceano?",
     "As belugas."
 )
