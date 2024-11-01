92478fe74f8bca1ee19b262a258455193c9765f3
Erica2D <147394713+Erica1C@users.noreply.github.com>
2024-11-01 08:42:10 -0300

    a

--- pergunta.js
+++ pergunta.js
@@ -1,24 +1,24 @@
 criarCartao(
     "Qual o maior animal aquático do mundo?",
-    "Baleia-azul."
+    "Baleia-azul"
 )
 criarCartao(
     "O que acontece se a Estrela-do-mar perder um de seus braços?",
-    "Ele se regenera."
+    "Ele se regenera"
 )
 criarCartao(
     "Por que os Polvos soltam uma espécie de tinta?",
-    "Técnica utilizada para fugir ou confundir predadores."
+    "Técnica utilizada para fugir ou confundir predadores"
 )
 criarCartao(
     "A Orca não é uma baleia. Qual é sua espécie?",
-    "As orcas são golfinhos."
+    "As orcas são golfinhos"
 )
 criarCartao(
     "Qual é o tipo de alimentação feita pelo Tubarão-Baleia?",
-    "Pequenos crustáceos, peixes que nadam em cardume e, ocasionalmente, atum e lulas."
+    "Pequenos crustáceos, peixes que nadam em cardume e, ocasionalmente, atum e lulas"
 )
 criarCartao(
     "Quais são as baleias mais dóceis do oceano?",
-    "As belugas."
+    "As belugas"
 )
